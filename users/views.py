a08779adb3406ba24d73acdd8b1ea3b058a21693
kazeem jamiu <kazeemj565@gmail.com>
2025-04-11 18:26:06 +0100

    feat(auth): implement user registration and login with JWT token response

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
