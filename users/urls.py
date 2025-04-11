080598c9e0c3f033afd8f8de7d336816f533f367
kazeem jamiu <kazeemj565@gmail.com>
2025-04-11 18:26:06 +0100

    feat(auth): implement user registration and login with JWT token response

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import RegisterView, LoginView
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+]
