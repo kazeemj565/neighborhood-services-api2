b19b84e2c33ddfe02ff8de7e7e28a0400110aae9
kazeem jamiu <kazeemj565@gmail.com>
2025-04-11 22:55:04 +0100

    feat: add serializers and API views for service listing with owner permissions

--- services/views.py
+++ services/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions
+from .models import Service
+from .serializers import ServiceSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class ServiceListCreateView(generics.ListCreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ServiceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
