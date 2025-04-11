013b333ac897b86eac37f618fec47a037f888f11
kazeem jamiu <kazeemj565@gmail.com>
2025-04-11 18:14:10 +0100

    chore(auth): setup JWT authentication using djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
     'rest_framework',
     'users',
     'services',
@@ -161,3 +162,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+
+
