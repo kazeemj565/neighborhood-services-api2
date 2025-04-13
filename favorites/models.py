016d03c4544f6f27bd2af8dfc9c072ea1f064244
kazeem jamiu <kazeemj565@gmail.com>
2025-04-13 19:12:39 +0100

    feat(favorites): add Favorite model and serializer for user-service relationship

--- favorites/models.py
+++ favorites/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.conf import settings
 from services.models import Service
 
+
 class Favorite(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
